70bc13bb3e21babe5519b0a743c29bbb73c55899
antwikwabena <92677998+antwikwabena@users.noreply.github.com>
2021-10-29 16:36:58 +0000

    another commit

--- project/patient/models.py
+++ project/patient/models.py
@@ -6,19 +6,40 @@ from datetime import datetime
 from sqlalchemy import join
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
-
+from project.staff.models import Staff
 
 class Patient (db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(30),nullable=False)
-    date_of_birth = db.Column(db.Date,nullable=False)
-    marital_status = db.Column(db.String(15))
-    patient_contact = db.Column(db.String(15))
-    emergency_contact = db.Column(db.String(15))
-    relationship_to_emergency_contact = db.Column(db.String(15))
-    gender = db.Column(db.String(10))
-    nhis_number = db.Column(db.String(30))
-    def __init__(self, name,date_of_birth,marital_status):
-        self.name = name
-        self.date_of_birth = date_of_birth
-        self.marital_status = marital_status
+    firstname = db.Column(db.String(30),nullable=False)
+    lastname = db.Column(db.String(30),nullable=False)
+    middlename = db.Column(db.String(30),nullable=False)
+    dateofbirth = db.Column(db.Date,nullable=False)
+    maritalstatus = db.Column(db.String(15),nullable=False)
+    phone = db.Column(db.String(15))
+    patientDate = db.Column(db.DateTime,nullable=False)
+    vitals_id = db.Column(db.Integer,db.ForeignKey('vitals.id'))
+    emergencyName = db.Column(db.String(30),nullable=False)
+    emergencyPhone = db.Column(db.String(15),nullable=False)
+    relationshiptoemergencycontact = db.Column(db.String(15))
+    sex = db.Column(db.String(10),nullable=False)
+    def __init__(self, firstname,middlename,lastname):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+
+class Vitals(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    patient_id = db.Column(db.Integer,db.ForeignKey('patient.id'),nullable=False)
+    staff_id = db.Column(db.Integer,db.ForeignKey('staff.id'),nullable=False)
+    weight = db.Column(db.Float(10),nullable=False)
+    height = db.Column(db.Float(10),nullable=False)
+    pulse = db.Column(db.Float(10),nullable=False)
+    respiratoryRate = db.Column(db.Float(10),nullable=False)
+    bloodPressureone = db.Column(db.Float(10),nullable=False)
+    bloodPressuretwo = db.Column(db.Float(10),nullable=False)
+    pulseOximeter = db.Column(db.Float(10),nullable=False)
+    calculatedBMI = db.Column(db.Float(10),nullable=False)
+    vitalsTime = db.Column(db.DateTime,nullable=False)
+    def __init__(self, patient_id,staff_id):
+        self.patient_id = patient_id
+        self.staff_id = staff_id
--- project/patient/views.py
+++ project/patient/views.py
@@ -6,6 +6,7 @@ from flask import send_file,make_response,flash,Blueprint,render_template, redir
 from project import db,app
 from flask_cors import CORS
 from project import ALLOWED_EXTENSIONS
+from project.patient.models import Patient
 from werkzeug.utils import secure_filename
 from project.staff.models import Staff
 from datetime import date,datetime,timedelta
@@ -18,3 +19,76 @@ from itertools import filterfalse
 from calendar import monthrange
 
 patient_blueprint = Blueprint('patient',__name__,template_folder='templates/patient',static_folder='static',static_url_path='/static/')
+
+@patient_blueprint.route('/add_patient',methods = ['GET','POST'])
+#@login_required
+def addPatient ():
+    title = 'Natural Solutions Herbal Clinic | Add Patient'
+    if request.method == 'POST':
+        if request.form['submit'] == 'addPatient':
+            firstname = request.form['firstname']
+            middlename= request.form['middlename']
+            lastname = request.form['lastname']
+            dateofbirth = request.form['dateofbirth']
+            sex = request.form['sex']
+            maritalstatus = request.form['maritalstatus']
+            address = request.form['address']
+            city = request.form['city']
+            region = request.form['region']
+            phone = request.form['phone']
+            emergencyName = request.form['emergencyName']
+            relationship = request.form['relationship']
+            emergencyPhone = request.form['emergencyPhone']
+            emergencyEmail = request.form['emergencyEmail']
+            newPatient = Patient(firstname,middlename,lastname)
+            newPatient.dateofbirth = dateofbirth
+            newPatient.sex = sex
+            newPatient.maritalstatus = maritalstatus
+            newPatient.phone = phone
+            newPatient.emergencyName = emergencyName
+            newPatient.emergencyPhone = emergencyPhone
+            db.session.add(newPatient)
+            db.session.commit()
+        else:
+            pass
+    else:
+        pass
+    return render_template('newPatient.html',title=title)
+
+
+@patient_blueprint.route('/capture_vitals',methods = ['GET','POST'])
+#@login_required
+def captureVitals ():
+    title = 'Natural Solutions Herbal Clinic | Capture Vitals'
+    all_patients = Patient.query.all()
+    if request.method == 'POST':
+        if request.form['submit'] == 'captureVitals':
+            weight = float(request.form['weight'])
+            height = float(request.form['height'])
+            calculatedBMI = float(request.form['calculatedBMI'])
+            pulse = float(request.form['pulse'])
+            pulseOximeter = float(request.form['pulseOximeter'])
+            temperature = float(request.form['temperature'])
+            respiratoryRate = float(request.form['respiratoryRate'])
+            bloodPressureone = float(request.form['bloodPressureone'])
+            bloodPressuretwo = float(request.form['bloodPressuretwo'])
+            pulseOximeter = float(request.form['pulseOximeter'])
+            patientCode = int(request.form['patientCode'])
+            patientVitals = Vitals(patientCode,1)
+            patientVitals.weight = weight
+            patientVitals.height = height
+            patientVitals.calculatedBMI = calculatedBMI
+            patientVitals.pulse = pulse
+            patientVitals.pulseOximeter = pulseOximeter
+            patientVitals.temperature = temperature
+            patientVitals.respiratoryRate = respiratoryRate
+            patientVitals.bloodPressureone = bloodPressureone
+            patientVitals.bloodPressuretwo = bloodPressuretwo
+            patientVitals.vitalsTime = datetime.utcnow()
+            db.session.add(patientVitals)
+            db.session.commit()
+        else:
+            pass
+    else:
+        pass
+    return render_template('captureVitals.html',all_patients=all_patients,title=title)
